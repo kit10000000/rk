57558935587580661c0e9c6bb62202b0ef3ce5e3
kit10000000 <ekaterina0196@yandex.ru>
2018-04-13 16:32:19 +0300

    deleted build

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -7,7 +7,6 @@
 //!
 int main(int argc, char** argv) {
     std::string text;
-
     std::string log_path = std::getenv("LOG_PATH"); /*!< установка переменной окружения */
     std::cout << log_path << std::endl;
 }
