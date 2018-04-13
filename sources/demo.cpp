d3562f2b4d18c61ba7040d0e2de755240ab86eb0
kit10000000 <ekaterina0196@yandex.ru>
2018-04-13 15:32:05 +0300

    added documentation

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
+//!
+//!установка переменной окружения
+//!
 int main(int argc, char** argv) {
     std::string text;
-    //!
-    //!установка переменной окружения
-    //!
+
     std::string log_path = std::getenv("LOG_PATH"); /*!< установка переменной окружения */
     std::cout << log_path << std::endl;
 }
